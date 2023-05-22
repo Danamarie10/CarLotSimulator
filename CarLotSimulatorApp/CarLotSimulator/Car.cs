be5a6267d6a3847f8c33c3930bfb0c38e5aec0bf
Danamarie10 <dana.patrick10@outlook.com>
2023-05-22 12:28:18 -0400

    finished exercise 2

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public string Make {get; set;}
+        public string Model { get; set;}
+        public int Year { get; set;}
+        public string EngineNoise { get; set;}
+        public string HonkNoise { get; set;}
+        public bool IsDrivable { get; set;}
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($"This {Make} {Model}'s engine sounds like {EngineNoise}");
+        }
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"This {Make} {Model}'s horn sounds like {HonkNoise}");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,7 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLot = new CarLot();
             //TODO
 
             //Create a seperate class file called Car
@@ -17,6 +18,35 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var car1 = new Car();
+            car1.Make = "Ford";
+            car1.Model = "Bronco";
+            car1.Year = 2015;
+            car1.IsDrivable = true;
+            car1.EngineNoise = "Vroom";
+            car1.HonkNoise = "Beep Beep";
+            carLot.ParkingLot.Add(car1);
+            var car2 = new Car();
+            car2.Make = "Honda";
+            car2.Model = "Civic";
+            car2.Year = 2023;
+            car2.IsDrivable = true;
+            car2.EngineNoise = "Vroom";
+            car2.HonkNoise = "Honk";
+            carLot.ParkingLot.Add(car2);
+            var car3 = new Car();
+            car3.Make = "Chevy";
+            car3.Model = "TrailBlazer";
+            car3.Year = 2018;
+            car3.IsDrivable = true;
+            car3.EngineNoise = "Vroom";
+            car3.HonkNoise = "Beep";
+            carLot.ParkingLot.Add(car3);
+
+            foreach(var car in carLot.ParkingLot)
+            {
+                Console.WriteLine($"This is a {car.Make} {car.Model} and the year is {car.Year}.");
+            }
 
             //*************BONUS*************//
 
